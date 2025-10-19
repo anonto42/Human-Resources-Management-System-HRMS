aa77bdcd914fa3a55dde22fbfe7abab0b3fa1727
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-19 17:07:01 +0600

    Config servicess added

---
+++ services/config-service/src/test/java/com/hrms/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
