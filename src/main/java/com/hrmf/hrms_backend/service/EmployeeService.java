d558faf5f098cbd20dbd04b917d9f4c2122ac9a9
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-05 14:12:19 +0600

    service's

---
+++ src/main/java/com/hrmf/hrms_backend/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.hrmf.hrms_backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    public final UserService userService;
+
+
+}
