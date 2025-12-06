e048aac1dcfb93fcdcf5ce4d357413cc7f1cce09
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-06 18:23:21 +0600

    added configuration properties

--- src/main/java/com/hrmf/hrms_backend/HrmsBackendApplication.java
+++ src/main/java/com/hrmf/hrms_backend/HrmsBackendApplication.java
@@ -2,8 +2,10 @@ package com.hrmf.hrms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class HrmsBackendApplication {
 
 	public static void main(String[] args) {
