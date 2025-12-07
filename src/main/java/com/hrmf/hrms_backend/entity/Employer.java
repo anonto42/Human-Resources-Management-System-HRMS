eb30a1196e228dcb6cf6f56b0c3dc636dbf9f7ea
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-07 17:11:13 +0600

    New Entity's added

---
+++ src/main/java/com/hrmf/hrms_backend/entity/Employer.java
@@ -0,0 +1,38 @@
+package com.hrmf.hrms_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "employers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @Column(name = "office_time")
+    private String officeTime;
+
+    @Column(nullable = false)
+    private String shift;
+
+    @Column(nullable = false)
+    private String number;
+}
