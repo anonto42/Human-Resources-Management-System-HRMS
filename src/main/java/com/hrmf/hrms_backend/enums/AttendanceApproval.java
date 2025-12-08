d40acd7769dc16ca34418bda2fc564b873b8389e
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-08 20:24:40 +0600

    enums added

---
+++ src/main/java/com/hrmf/hrms_backend/enums/AttendanceApproval.java
@@ -0,0 +1,5 @@
+package com.hrmf.hrms_backend.enums;
+
+public enum AttendanceApproval {
+    PENDING, APPROVED, REJECTED
+}
---
+++ src/main/java/com/hrmf/hrms_backend/enums/AttendanceStatus.java
@@ -0,0 +1,5 @@
+package com.hrmf.hrms_backend.enums;
+
+public enum AttendanceStatus {
+    CHECKED_IN, ON_BREAK, PAUSED, CHECKED_OUT, ABSENT, HALF_DAY
+}
