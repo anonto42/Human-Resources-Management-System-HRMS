63e5a22a4f19f996f9d01eb5de6e3ef9bbc412c6
Sohidul Islam Ananto <anontom90@gmail.com>
2025-12-23 16:20:50 +0600

    Have to add the test on the full application

---
+++ src/test/java/com/hrmf/hrms_backend/controller/auth/AuthControllerTest.java
@@ -0,0 +1,16 @@
+package com.hrmf.hrms_backend.controller.auth;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Auth Controller Test")
+class AuthControllerTest {
+
+    @Test
+    @DisplayName("User login test")
+    void login (){}
+
+}
